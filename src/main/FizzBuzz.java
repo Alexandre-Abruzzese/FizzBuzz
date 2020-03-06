072590a008e1e95a7138df1177104ca28af92006
Alex <alex.abruzzese96@gmail.com>
2020-03-06 12:08:50 +0100

    red check number if divisibleby 3 or 5

---
+++ src/main/FizzBuzz.java
@@ -0,0 +1,9 @@
+package main;
+
+public class FizzBuzz
+{
+    public String fizz_buzz(int number_to_split)
+    {
+        return "ok";
+    }
+}
---
+++ src/test/UnitTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import main.FizzBuzz;
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class UnitTest
+{
+    private static final FizzBuzz fizzbuzz = new FizzBuzz();
+    @Test
+    public void should_return_empty_string_if_number_is_not_splitable()
+    {
+        int numberToSplit = 40;
+        assertThat(fizzbuzz.fizz_buzz(numberToSplit), equalTo(""));
+    }
+}
