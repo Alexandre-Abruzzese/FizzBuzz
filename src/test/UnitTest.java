127e6cd1459b726cca2f37ada0eb9252eaf46ad8
Alex <alex.abruzzese96@gmail.com>
2020-03-06 12:22:36 +0100

    refractor variable global

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -9,18 +9,17 @@ import static org.junit.Assert.assertThat;
 public class UnitTest
 {
     private static final FizzBuzz fizzbuzz = new FizzBuzz();
+    public int numberToSplit = 30;
 
     @Test
     public void should_return_empty_string_if_number_is_not_divisible()
     {
-        int numberToSplit = 40;
         assertThat(fizzbuzz.fizz_buzz(numberToSplit), equalTo(""));
     }
 
     @Test
     public void should_return_fizz_if_number_is_divisible_by_three()
     {
-        int numberToSplit = 3;
         assertThat(fizzbuzz.fizz_buzz(numberToSplit), equalTo("fizz"));
     }
 }
